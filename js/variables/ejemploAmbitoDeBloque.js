317f2f1341e70fc8e1530342ed3d80c12a34cf16
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2023-09-23 05:00:17 -0600

    feat: add block scope in /js/variables topic

---
+++ js/variables/ejemploAmbitoDeBloque.js
@@ -0,0 +1,12 @@
+if (true) {
+  let nombre = "Jaime";
+}
+console.log(nombre); // Error: nombre no está definido ❌🚫
+
+{
+  const numeroMagico = 42;
+}
+console.log(numeroMagico); // Error: numeroMagico no está definido ❌🔮
+
+const gravedad = 9.81;
+gravedad = 10; // Error: No puedes cambiar una constante ❌🛑
